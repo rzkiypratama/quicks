7a6ba40946da96ba86444a79c231cbcb93dbea5e
quicks-be <booking.system404@gmail.com>
2023-12-21 23:29:43 +0800

    fix: fix icon position and styling

--- app/component/SearchBar.tsx
+++ app/component/SearchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar: React.FC = () => {
   return (
     <div className={styles.searchBars}>
       <img src='assets/search.png' alt="Search" className={styles.searchIcons} />
-      <input type="text" placeholder="Search..." className={styles.searchInputs} />
+      <input type="text" className={styles.searchInputs} />
     </div>
   );
 };
